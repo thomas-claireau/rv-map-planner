c9b1f80638c021076b312656e32d4dd9c1ea9af9
Thomas Claireau <thomas.claireau@gmail.com>
2021-11-16 18:11:52 +0100

    remove useless config

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,11 +1,10 @@
-import axios from 'axios';
 import { SocialProfileJsonLd } from 'next-seo';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import Layout from '../components/Layout/Layout';
 import Map from '../components/Map/Map';
 
-export default function Home({ fields, global }) {
+export default function Home() {
 	const [google, setGoogle] = useState(null);
 
 	useEffect(() => {
@@ -21,8 +20,6 @@ export default function Home({ fields, global }) {
 			<SocialProfileJsonLd
 				type="Person"
 				name="Thomas Claireau"
-				url={global?.seo.home_url}
-				sameAs={global?.header.menus.items.map((item) => item.url)}
 			/>
 
 			<Map google={google} />
@@ -33,35 +30,3 @@ export default function Home({ fields, global }) {
 Home.propTypes = {
 	fields: PropTypes.object,
 };
-
-export async function getServerSideProps() {
-	const auth = {
-		auth: {
-			username: process.env.NEXT_PUBLIC_API_USERNAME,
-			password: process.env.NEXT_PUBLIC_API_PASSWORD,
-		},
-	};
-
-	let fields, global;
-
-	try {
-		fields = await axios.get(
-			`${process.env.NEXT_PUBLIC_API_URL}/pages?post_id=22`,
-			auth
-		);
-
-		global = await axios.get(
-			`${process.env.NEXT_PUBLIC_API_URL}/global`,
-			auth
-		);
-	} catch (error) {
-		console.error(error);
-	}
-
-	return {
-		props: {
-			fields: fields?.data ?? null,
-			global: global?.data ?? null,
-		},
-	};
-}
