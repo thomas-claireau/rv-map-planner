f0b0b86909a76dbe0ff9db430605d26a5f7dba96
Thomas Claireau <thomas.claireau@gmail.com>
2021-11-16 14:31:58 +0100

    add locations table

---
+++ backend/migrations/20211116132933-create-location.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      lat: {
+        type: Sequelize.INTEGER
+      },
+      lng: {
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Locations');
+  }
+};
--- backend/models/location.js
+++ backend/models/location.js
@@ -1,29 +1,25 @@
 'use strict';
-const { Model } = require('sequelize');
-
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-	class Location extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			// define association here
-		}
-	}
-
-	Location.init(
-		{
-			lat: DataTypes.INTEGER,
-			lng: DataTypes.INTEGER,
-			address: DataTypes.STRING,
-		},
-		{
-			sequelize,
-			modelName: 'Location',
-		}
-	);
-
-	return Location;
+  class Location extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Location.init({
+    lat: DataTypes.INTEGER,
+    lng: DataTypes.INTEGER,
+    address: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Location',
+  });
+  return Location;
 };
